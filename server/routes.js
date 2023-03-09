5ddedcce82eb9ad725ca37a821b7051cd85d4583
Jennifer Harper <jennnnifer.harper@gmail.com>
2023-03-09 16:17:09 +1300

    initial commit

---
+++ server/routes.js
@@ -0,0 +1,59 @@
+const fs = require('node:fs/promises')
+const express = require('express')
+const router = express.Router()
+
+
+
+router.get('/:id', (req, res) => {
+  const id = Number(req.params.id)
+
+  fs.readFile(__dirname + '/data/data.json', 'utf-8')
+    .then((data) => {
+      const parsedAnswer = JSON.parse(data)
+      const theOne = parsedAnswer.genres.find((element) => element.id === id)
+      res.render('edit', theOne)
+    })
+    .catch((err) => {
+      res.status(500).send(err.message)
+    })
+})
+
+
+//get the form and the user input and use fs.writeFile to update json
+router.post('/:id/edit', (req, res) => {
+  const formAnswer = req.body
+
+  //return the id as a number, not a string
+  const id = Number(req.params.id)
+
+  fs.readFile(__dirname + '/data/data.json', 'utf-8')
+    .then((data) => {
+      //get all the genres
+      const parsedAnswer = JSON.parse(data)
+      //get all the pups and find the one that matches my edit page id
+      const oldAnswer = parsedAnswer.genres.find((element) => element.id === id)
+
+      //get the breed from oldAnswer and change it to what my form said
+      oldAnswer.name = formAnswer.name
+      // console.log(oldAnswer)
+      // console.log(parsedAnswer)
+
+      //change the json data so its readable, and align back to object
+      const stringAnswer = JSON.stringify(parsedAnswer, null, 2)
+      // console.log(stringAnswer)
+
+      return fs.writeFile(__dirname + '/data/data.json', stringAnswer)
+    })
+
+    .then(() => res.redirect(`/results`))
+
+    .catch((err) => {
+      res.status(500).send(err.message)
+    })
+})
+
+
+
+
+
+module.exports = router
---
+++ server/server.js
@@ -0,0 +1,57 @@
+const fs = require('node:fs/promises')
+const express = require('express')
+const hbs = require('express-handlebars')
+
+const fortuneRoutes = require('./routes')
+
+const server = express()
+
+// Server configuration
+const publicFolder = __dirname + '/public'
+server.use(express.static(publicFolder))
+server.use(express.urlencoded({ extended: false }))
+
+// Handlebars configuration
+server.engine('hbs', hbs.engine({ extname: 'hbs' }))
+server.set('view engine', 'hbs')
+server.set('views', __dirname + '/views')
+
+server.use('/fortune', fortuneRoutes)
+
+// Your routes/router(s) should go here
+
+server.get('/', (req, res) => {
+  fs.readFile(__dirname + '/data/data.json', 'utf-8')
+    .then((data) => {
+    let thing = JSON.parse(data)
+  
+    let dataObj = {
+      page_title: 'The Wisdom of Zoltash',
+      //first part shows in {{}} in html
+      //thing.xx is the array name
+      show_genres: thing.genres
+    }
+      res.render('home', dataObj)
+    })
+    .catch((err) => {
+      res.status(500).send(err.message)
+    })
+})
+
+server.get('/results', (req, res) => {
+  fs.readFile(__dirname + '/data/data.json', 'utf-8')
+    .then((data) => {
+    let thing = JSON.parse(data)
+  
+    let dataObj = {
+      page_title: 'Fortune Time',
+      show_genres: thing.genres
+    }
+      res.render('results', dataObj)
+    })
+    .catch((err) => {
+      res.status(500).send(err.message)
+    })
+})
+
+module.exports = server
