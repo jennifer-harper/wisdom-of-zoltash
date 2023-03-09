b70bc9d412f07c62e0b5dd874f27fdd39777da6d
Jennifer Harper <jennnnifer.harper@gmail.com>
2023-03-09 17:18:09 +1300

    ready for results work

--- server/routes.js
+++ server/routes.js
@@ -11,7 +11,15 @@ router.get('/:id', (req, res) => {
     .then((data) => {
       const parsedAnswer = JSON.parse(data)
       const theOne = parsedAnswer.genres.find((element) => element.id === id)
-      res.render('edit', theOne)
+
+      let dataObj ={
+        page_title:"Get your answers",
+        selected_genre: theOne,
+        
+      }
+      console.log(theOne)
+
+      res.render('edit', dataObj)
     })
     .catch((err) => {
       res.status(500).send(err.message)
@@ -22,10 +30,8 @@ router.get('/:id', (req, res) => {
 //get the form and the user input and use fs.writeFile to update json
 router.post('/:id/edit', (req, res) => {
   const formAnswer = req.body
-
   //return the id as a number, not a string
   const id = Number(req.params.id)
-
   fs.readFile(__dirname + '/data/data.json', 'utf-8')
     .then((data) => {
       //get all the genres
@@ -37,11 +43,9 @@ router.post('/:id/edit', (req, res) => {
       oldAnswer.name = formAnswer.name
       // console.log(oldAnswer)
       // console.log(parsedAnswer)
-
       //change the json data so its readable, and align back to object
       const stringAnswer = JSON.stringify(parsedAnswer, null, 2)
       // console.log(stringAnswer)
-
       return fs.writeFile(__dirname + '/data/data.json', stringAnswer)
     })
 
